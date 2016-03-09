ce7f241075be8c0a86013cde6b137f84163d0968
HyrekanDragon <HyrekanDragon@yahoo.com>
2016-03-08 21:44:11 -0800

    Create Recursion.h

---
+++ Solution/HyrekanDragon/Recursion.h
@@ -0,0 +1,30 @@
+/* ----------------------------------------------------------------------------
+* Copyright &copy; 2016 Alex Chuoy <HyrekanDragon@yahoo.com>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
+
+/**
+* Function prototypes for testing recursion.
+*/
+
+#ifndef RECURSION_H
+#define RECURSION_H
+#include <string>
+
+using std::string;
+
+int gcd(int a, int b);
+
+int fib(int n);
+
+int pow(int a, int b);
+
+int tri(int n);
+
+string int_to_roman(int n);
+
+string int_to_words(int n);
+
+string magic_number(int n);
+
+#endif
