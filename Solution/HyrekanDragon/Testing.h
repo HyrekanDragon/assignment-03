f011475a2c43a4cc64589d2f03ce1be169ac02cf
HyrekanDragon <HyrekanDragon@yahoo.com>
2016-03-08 21:43:07 -0800

    Create Testing.h

---
+++ Solution/HyrekanDragon/Testing.h
@@ -0,0 +1,48 @@
+/* ----------------------------------------------------------------------------
+* Copyright &copy; 2016 Alex Chuoy <HyrekanDragon@yahoo.com>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
+
+/**
+* Function prototypes for testing exceptions, templates and the vector, set and map STL.
+*/
+
+#ifndef TESTING_H
+#define TESTING_H
+
+template <typename D>
+class Dragon
+{
+private:
+	D number;
+public:
+	void setNumber(D newNumber)
+	{
+		number = newNumber;
+	}
+	D getNumber() const
+	{
+		return number;
+	}
+	void speak() const
+	{
+		cout << "This dragon is holding the number "
+			<< number
+			<< endl;
+	}
+};
+
+void test_exceptions();
+
+void test_templates();
+
+void test_stl();
+
+template <typename t>
+t divide(t a, t b);
+
+template <typename t>
+void swap(t & a, t & b);
+
+
+#endif
