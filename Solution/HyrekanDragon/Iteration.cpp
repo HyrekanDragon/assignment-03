a846f1f1674731d4c0a9e3ce14866679f3e1e91d
HyrekanDragon <HyrekanDragon@yahoo.com>
2016-03-08 21:45:30 -0800

    Create Iteration.cpp

---
+++ Solution/HyrekanDragon/Iteration.cpp
@@ -0,0 +1,94 @@
+/* ----------------------------------------------------------------------------
+* Copyright &copy; 2016 Alex Chuoy <HyrekanDragon@yahoo.com>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
+
+/**
+* Functions for rewriting recursion functions into iteration functions.
+*/
+
+
+#include "Iteration.h"
+#include <iostream>
+#include <exception>
+#include <string>
+
+using std::cout;
+using std::endl;
+
+int gcd_iter(int a, int b)
+{
+	//Normalization of data
+	if (a < 0) a *= -1;
+	if (b < 0) b *= -1;
+
+	//Exception
+	if (a == 0 && b == 0) throw 'a';
+
+	//Iteration
+	while (a != 0 && b != 0)
+	{
+		if (a > b) a -= b;
+		else	   b -= a;
+	}
+
+	if (a == 0) return b;
+	if (b == 0) return a;
+}
+
+int fib_iter(int n)
+{
+	//Exception
+	if (n <= 0) throw 'b';
+
+	//Iteration
+	if (n == 1 || n == 2) return 1;
+
+	int a = 1, b = 1, temp;
+
+	for (int i = 3; i <= n; i++)
+	{
+		temp = b;
+		b += a;
+		a = temp;
+	}
+
+	return b;
+}
+
+int pow_iter(int a, int b)
+{
+	int pow = a;
+
+	//Exception
+	if (b < 0) throw 'c';
+	if (a == 0 && b == 0) throw 'c';
+
+	//Special Case
+	if (b == 0) return 1;
+	if (b == 1) return a;
+	
+	//Iteration
+	for (int i = 2; i <= b ; i++)
+	{
+		pow *= a;
+	}
+
+	return pow;
+}
+
+int tri_iter(int n)
+{
+	int sum = 0;
+
+	//Exception
+	if (n < 1) throw 'd';
+
+	//Iteration
+	for (int i = 1; i <= n; i++)
+	{
+		sum += i;
+	}
+
+	return sum;
+}
